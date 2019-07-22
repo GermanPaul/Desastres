d94c87154e540d2c973517b537b74139af3de90d
German Pretelt <germanpaul14@gmail.com>
2019-07-21 22:29:54 -0500

    Eliminada geolocalización local, reemplazada por Bing API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const geocoder = require('local-reverse-geocoder');
-
+// const geocoder = require('local-reverse-geocoder');
 
 const app = express();
 
@@ -11,9 +10,9 @@ const { mongoose } = require('./database');
 
 // Settings 
 app.set('port', process.env.PORT || 3000);
-geocoder.init({}, function() {
-  // geocoder is loaded and ready to run
-});
+// geocoder.init({}, function() {
+//   // geocoder is loaded and ready to run
+// });
 
 // Middlewares
 app.use(morgan('dev'));
@@ -27,16 +26,16 @@ app.get('/reportar',function(req,res) {
 app.get('/alertas',function(req,res) {
   res.sendFile(__dirname + '/public/alertas.html');
 });
-app.get('/geo/:lat/:long',function(req,res) {
-  // console.log('Peticion recibida: ');
-  const point = {latitude: req.params.lat, longitude: req.params.long};
-  // console.log(point);
-  geocoder.lookUp(point, function(err, res1) {
-    const respuesta = JSON.parse(JSON.stringify(res1, null, 2));
-    res.json(respuesta);
-  });
-  // res.json(point);
-});
+// app.get('/geo/:lat/:long',function(req,res) {
+//   // console.log('Peticion recibida: ');
+//   const point = {latitude: req.params.lat, longitude: req.params.long};
+//   // console.log(point);
+//   geocoder.lookUp(point, function(err, res1) {
+//     const respuesta = JSON.parse(JSON.stringify(res1, null, 2));
+//     res.json(respuesta);
+//   });
+//   // res.json(point);
+// });
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));;
